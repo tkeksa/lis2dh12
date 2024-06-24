030919bdf1286bda4d5da93ede0fe95f3ebfb10e
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2024-06-24 16:41:14 +0200

    update embedded-hal dependency to v1.0.0

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,7 +1,7 @@
 pub use lis2dh12::{Lis2dh12, SlaveAddr};
 
 use embedded_hal_mock as hal;
-pub use hal::i2c::{Mock as I2cMock, Transaction as I2cTrans};
+pub use hal::eh1::i2c::{Mock as I2cMock, Transaction as I2cTrans};
 
 pub const DEV_ADDR: u8 = 0b001_1000;
 
